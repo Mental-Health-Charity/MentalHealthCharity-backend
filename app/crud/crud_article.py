d2502d63d7766f2cfa7fc4dcb8dc9822cc4e6663
Apriano <aprzybylski54@gmail.com>
2023-06-14 20:40:44 +0200

    update logic for creating articles

--- app/crud/crud_article.py
+++ app/crud/crud_article.py
@@ -8,6 +8,22 @@ from app.schemas.article import ArticleCreate, ArticleUpdate
 
 
 class CRUDArticle(CRUDBase[Article, ArticleCreate, ArticleUpdate]):
+
+    def create(self, db: Session, obj_in: ArticleCreate, **kwargs) -> Article:
+        user = kwargs.get("user")
+        db_obj = Article(
+            created_by_id=user.id,
+            required_role=obj_in.required_role,
+            title=obj_in.title,
+            content=obj_in.content,
+            banner_url=obj_in.banner_url
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+
     def get_all(self, db: Session):
         return db.query(Article).filter()
 
