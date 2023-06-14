ea8dee7e00383e0bb3b3aebd11ec004c2650752c
Apriano <aprzybylski54@gmail.com>
2023-06-14 20:15:14 +0200

    update settings

--- app/core/config.py
+++ app/core/config.py
@@ -28,24 +28,9 @@ class Settings(BaseSettings):
         raise ValueError(v)
 
     PROJECT_NAME: str = "mentalhealth"
-    MYSQL_CONNECTOR: str = os.environ["DATABASE_CONNECTOR"]
-    MYSQL_SERVER: str = os.environ["DATABASE_ADDRESS"]
-    MYSQL_USER: str = os.environ["DATABASE_USER"]
-    MYSQL_PASSWORD: str = os.environ["DATABASE_PASSWORD"]
-    MYSQL_DB: str = os.environ["DATABASE_DB"]
     SQLALCHEMY_DATABASE_URI: Optional[str] = os.environ["DATABASE_URL"]
 
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
-        return "{}://{}:{}@{}:3306/{}".format(
-            values.get("MYSQL_CONNECTOR"),
-            values.get("MYSQL_USER"),
-            values.get("MYSQL_PASSWORD"),
-            values.get("MYSQL_SERVER"),
-            values.get("MYSQL_DB"),
-        )
+
 
     FIRST_SUPERUSER: EmailStr = os.environ["SUPERUSER_EMAIL"]
     FIRST_SUPERUSER_PASSWORD: str = os.environ["SUPERUSER_PASSWORD"]
